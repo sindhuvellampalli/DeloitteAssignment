a872d4d13a4f0173439b73ee2c20877ef769cfc5
SaiSindhu Vellampalli <saisindhu.vellampalli@dxc.com>
2020-08-10 14:45:08 +0530

    Deloitte Code

---
+++ js/itemList.js
@@ -0,0 +1,415 @@
+itemList = [
+    {
+        "id": 1,
+        "name": "Kappu Regular Sofa",
+        "collection": "coombes",
+        "category": "LOUNGE",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-1.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Kappu regular",
+        "collection": "Book-Shelf",
+        "category": "SOFA",
+        "price": "$123",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-5.jpg"
+    },
+    {
+        "id": 3,
+        "name": "sindhu22",
+        "collection": "Momo",
+        "category": "ARMCHAIR",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-3.jpg"
+    },
+    {
+        "id": 4,
+        "name": "sindhu22",
+        "collection": "SHELVES",
+        "category": "sofa",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-5.jpg"
+    },
+    {
+        "id": 5,
+        "name": "sindhu22",
+        "collection": "Penemille",
+        "category": "CHAIR",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-3.jpg"
+    },
+    {
+        "id": 6,
+        "name": "sindhu22",
+        "collection": "Nille",
+        "category": "SOFA",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-5.jpg"
+    },
+    {
+        "id": 7,
+        "name": "sindhu22",
+        "collection": "Nille",
+        "category": "SOFA",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-1.jpg"
+    },
+    {
+        "id": 8,
+        "name": "sindhu22",
+        "collection": "Nille",
+        "category": "SOFA",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-3.jpg"
+    },
+    {
+        "id": 9,
+        "name": "sindhu22",
+        "collection": "Nille",
+        "category": "SOFA",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-5.jpg"
+    },
+    {
+        "id": 10,
+        "name": "sindhu22",
+        "collection": "Nille",
+        "category": "SOFA",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-2.jpg"
+    },
+    {
+        "id": 11,
+        "name": "sindhu22",
+        "collection": "Nille",
+        "category": "SOFA",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-4.jpg"
+    },
+    {
+        "id": 12,
+        "name": "sindhu22",
+        "collection": "Nille",
+        "category": "SOFA",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-2.jpg"
+    },
+    {
+        "id": 13,
+        "name": "sindhu22",
+        "collection": "Nille",
+        "category": "SOFA",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-3.jpg"
+    },
+    {
+        "id": 14,
+        "name": "sindhu22",
+        "collection": "Nille",
+        "category": "SOFA",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-1.jpg"
+    },
+    {
+        "id": 15,
+        "name": "sindhu22",
+        "collection": "Nille",
+        "category": "SOFA",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-3.jpg"
+    },
+    {
+        "id": 16,
+        "name": "sindhu22",
+        "collection": "Nille",
+        "category": "SOFA",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-1.jpg"
+    },
+    {
+        "id": 17,
+        "name": "sindhu22",
+        "collection": "Nille",
+        "category": "SOFA",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-3.jpg"
+    },
+    {
+        "id": 18,
+        "name": "sindhu22",
+        "collection": "Nille",
+        "category": "SOFA",
+        "price": "$1234",
+        "rating": 4,
+        "description": "Vass shoes makeshandcrafted mens shoes in heart of Budapest. I made a concept product page for practice which im my opinion represents their qualities better than their current site",
+        "colors": [
+            "black",
+            "brown"
+        ],
+        "img": [
+            "product1.jpg"
+        ],
+        "displayImage": "images/shop/living-1.jpg"
+    }
+]
+
+index = 0
+itemsList = itemList;
+selectedItem = itemsList[0];
+
+// generate itemList template
+function createCards() {
+    var items = '';
+    if (itemsList.length > 0) {
+        itemsList.forEach(element => {
+
+            index = element.id;
+
+            items += '<div class="col-sm-4 list" onclick="itemClicked(' + index + ')" ><div class="product-image-wrapper"><div class="single-products"><div class="productinfo text-center">';
+            items += '<img  id=' + element.id + ' alt="" />';
+            items += '<p class="item-name">' + element.collection + '</p>';
+            items +=
+                '<div class="product-price">   <label class="item-info">' + element.category + '</label><label class="price-info">' + element.price + '</label></div>';
+            items += ' <div class="product-price"><div><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star unchecked"></span></div>';
+            items += '    <a class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i></a></div></div></div></div></div>';
+
+        });
+    }
+    else {
+        items = '<label>No records found</label>'
+    }
+    var itemParent = document.getElementById('parentCard');
+    itemParent.innerHTML = items;
+    itemsList.forEach(element => {
+        var item = document.getElementById(element.id);
+        item.src = element.displayImage;
+    });
+}
+
+// generate itemInfo template
+function onItemSelect() {
+    var itemInfo = `<div class="col-lg-6 col-md-6 col-sm-6">
+            <img id=`+ selectedItem.id + `  />
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 mt-5rem">
+            <div class="item-info">`+ selectedItem.name + `</div>
+            <div class="Collection-info">`+ selectedItem.category + `</div>
+            <div class="item-description">`+ selectedItem.description + `</div>
+            <div>Price per unit </div>
+            <div class="checkout">
+                <label class="item-price">`+ selectedItem.price + `</label>
+                <button name="buynow" class="btn btn-default submit-btn-ref buttons-common-ref-all">BUY NOW</button>
+                <a  class="btn btn-default add-cart"><i class="fa fa-shopping-cart"></i></a>
+
+            </div>
+        </div>
+`;
+    document.getElementById(selectedItem.id).src = selectedItem.displayImage;
+    document.getElementById('parentInfo').innerHTML = itemInfo;
+
+}
+
+// display item functionality
+function itemClicked(index) {
+    console.log("hello", index)
+    selectedItem = itemList.find(element => {
+        return element.id === index
+    });
+    parent.document.getElementById('itemscontent').src = "./itemInfo.html";
+}
+
+// pagination functionality
+var currentPage = 1;
+function showrecords(index) {
+    if (index === 0 && currentPage > 1) {
+        currentPage -= 1;
+    }
+    if (index === 10 && currentPage > 3) {
+        currentPage += 1;
+    } else {
+        currentPage = index;
+    }
+
+    switch (currentPage) {
+        case 1:
+            itemsList = itemList.slice(0, 6);
+            break;
+        case 2:
+            itemsList = itemList.slice(6, 12);
+            break;
+        case 3:
+            itemsList = itemList.slice(12, itemList.length);
+            break;
+        default:
+            break;
+    }
+    createCards();
+}
+
+// Filter functinality
+function onsearchText() {
+    var searchText = document.getElementById('searchText').value;
+    itemsList = itemList.filter(element => {
+        if (element.category.indexOf(searchText) != -1) {
+            return element.category.includes('searchText');
+        }
+    });
+
+    createCards();
+
+}
+
